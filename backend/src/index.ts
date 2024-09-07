411f09d497cce2728cee55ad96f032f7933489ca
10-abhi <abhishek81936@gmail.com>
2024-09-07 20:18:48 +0530

    addded jwt for authentication

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,45 +1,60 @@
 import { Hono } from 'hono'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-
+import { sign } from 'hono/jwt';
 
 const app = new Hono<{
   Bindings: {
-    DATABASE_URL: string
+    DATABASE_URL: string,
+    JWT_SECRET: string
   }
 }>();
 
-app.get('/api/v1/user/signup', (c) => {
+app.post('/api/v1/user/signup', async (c) => {
+  
   const prisma = new PrismaClient({
-    datasourceUrl: c.env?.DATABASE_URL,
+   datasourceUrl: c.env?.DATABASE_URL,
   }).$extends(withAccelerate())
   
-  const body = c.req.json();
-
-   
+  const body =  await c.req.json();
+ 
+ 
+  try{
+    const user = await prisma.user.create({
+      data:{
+        email: body.email,
+        password: body.password
+      }
+    })
+    const jwt = await sign({ id: user.id }  , c.env.JWT_SECRET);
+    return c.json({jwt});
+  } catch(e){
+     c.status(403);
+     return c.json({e : "error while signing up"})
+  }
   return c.text('Hello Hono!')
 })
 
-app.get('/api/v1/user/signin', (c) => {
+// app.get('/api/v1/user/signin', (c) => {
 
-  return c.text('Hello Hono!')
+//   return c.text('Hello Hono!')
   
-})
+// })
 
-app.get('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
+// app.get('/api/v1/blog', (c) => {
+//   return c.text('Hello Hono!')
+// })
 
-app.get('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
+// app.get('/api/v1/blog', (c) => {
+//   return c.text('Hello Hono!')
+// })
 
-app.get('/api/v1/blog/:id', (c) => {
-  return c.text('Hello Hono!')
-})
+// app.get('/api/v1/blog/:id', (c) => {
+//   return c.text('Hello Hono!')
+// })
 
-app.get('/api/v1/blog/bulk', (c) => {
-  return c.text('Hello Hono!')
-})
+// app.get('/api/v1/blog/bulk', (c) => {
+//   return c.text('Hello Hono!')
+// })
 
 export default app
