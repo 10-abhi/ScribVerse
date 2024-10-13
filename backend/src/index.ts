ddef7e5ab6e72d69462a4913e17804772e52ba7e
10-abhi <abhishek81936@gmail.com>
2024-10-13 16:35:39 +0530

    adding cors middleware of hono

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono'
 import blogRoute from './routes/blog';
 import userRoute from './routes/user';
+import { cors } from 'hono/cors';
+
 
 const app = new Hono<{}>();
-
+app.use('/*' , cors());
 app.route('api/v1/user' , userRoute);
 app.route('api/v1/blog' , blogRoute);
 
