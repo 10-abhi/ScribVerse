b79e2168ece979b86006807c9011593b797e15b7
10-abhi <abhishek81936@gmail.com>
2025-07-02 17:56:18 +0530

    added route to generate content

--- backend/src/routes/airoute.ts
+++ backend/src/routes/airoute.ts
@@ -41,3 +41,33 @@ airoute.get("get-topics", async (c) => {
         return c.json({ error: "Failed to generate topics" }, 500);
     }
 })
+
+airoute.get("generate-content" , async(c)=>{
+    try {
+      const title = c.req.param("title");
+      if(!title){
+        return c.json({ error: "Failed to get the title" }, 500);
+      }
+      const openai = new OpenAI({
+        baseURL : 'https://openrouter.ai/api/v1',
+        apiKey:c.env.aiapikey
+      })
+      if(!openai){return}
+      const completion = await openai.chat.completions.create({
+        model:"gpt-3.5-turbo",
+        messages:[
+            {
+                role:"user",
+                content : `Write a detailed blog post (~500 words) on the topic: "${title}". Make it engaging and well-structured.`,
+            }
+        ]
+      })
+      if(!completion){return c.json({ error: "Failed to generate the content for this topic" }, 500);}
+      const blogcontent = completion.choices[0].message.content;
+      return c.json({content : blogcontent});
+
+    } catch (error) {
+        console.error(error);
+        return c.json({error : "Failed to generate content"}, 500);
+    }
+})
